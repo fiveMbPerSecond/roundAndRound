62e0085f664ba6ec7a145a1e6b6faaad2d807029
Trent Moyar <trentmoyar@gmail.com>
2020-04-30 23:03:34 -0400

    New improved multiply

--- multiply.py
+++ multiply.py
@@ -1,3 +1,5 @@
+from math import *
+
 
 #function that multiplies two integers
 #highly optimised
@@ -11,3 +13,6 @@ def multiply(x,y):
 #returns x grteater than or equal to y
 def greater_than_or_equal(x,y):
    return int(x/y)
+
+def better_multiply(x,y):
+   return exp(log(x) + log(y))
