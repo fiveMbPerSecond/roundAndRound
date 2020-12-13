5ca406903bd4c3e032f877ffdc4d6c5984062522
Alex Whitlock <alexwhitlock@Alexs-MacBook-Pro.local>
2020-12-12 21:00:18 -0500

    added favicon

--- test.py
+++ test.py
@@ -5,17 +5,17 @@ headers = {
     'Ocp-Apim-Subscription-Key': 'dd7576412024481e88a51e02c3803147',
 }
 params = urllib.parse.urlencode({
-    # Request parameters
-    # '$format': '{String}',
-    # '$filter': '{String}',
-    # '$select': '{String}',
-    # '$top': '{string}',
-    # '$skip': '{string}',
-    # '$orderby': '{String}',
+    'lat1': '29.728379',
+    'lon1': '-95.349847',
+    'lat2': '29.738815',
+    'lon2': '-95.346083',
+    # # '$format': '{String}',
+    # # '$orderby': '{String}',
+    # 'endTime': "datetime'2020-12-13T7:00:00Z'",
 })
 try:
     conn = http.client.HTTPSConnection('hacktj2020api.eastbanctech.com')
-    conn.request("GET", "/transitiq/Vehicles?%s" % params, "{body}", headers)
+    conn.request("GET", "CalculateItineraryByPoints?%s" % params, "{body}", headers)
     response = conn.getresponse()
     data = response.read()
     print(data)
